1ddf535d982d087ffea5f30ec3d9c96a29cdba12
Harsh SIngh <hi.harsh@protonmail.ch>
2021-07-06 23:27:29 -0400

    Added posts section

--- abell.config.js
+++ abell.config.js
@@ -3,7 +3,7 @@ module.exports = {
   contentPath: 'content', // Content of the blog is in 'content' directory.
   outputPath: 'dist', // On npm run build, website is created in 'dist' directory.
   globalMeta: {
-    title: 'Abell Default Template',
+    title: 'Harsh Singh',
     shortName: 'Abell'
   }
 };
