6042617c1cafb9fea911a66b9262ea2869c0c326
Harsh SIngh <hi.harsh@protonmail.ch>
2021-07-06 20:31:33 -0400

    Init project

---
+++ abell.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  themePath: 'theme', // Source files of the blog in 'theme' directory.
+  contentPath: 'content', // Content of the blog is in 'content' directory.
+  outputPath: 'dist', // On npm run build, website is created in 'dist' directory.
+  globalMeta: {
+    title: 'Abell Default Template',
+    shortName: 'Abell'
+  }
+};
